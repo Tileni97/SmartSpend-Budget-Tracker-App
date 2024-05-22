86f15fdf582a0437b26f9ed60cb31cf3fa5a660c
Giideon Victory <shikongov02@gmail.com>
2024-05-22 02:50:10 +0200

    transaction activity

--- SmartSpend/app/src/main/java/com/example/smartspend/Screens/home/TransactionsActivity.kt
+++ SmartSpend/app/src/main/java/com/example/smartspend/Screens/home/TransactionsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smartspend.Screens.home
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -12,8 +13,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountBalanceWallet
@@ -21,6 +25,7 @@ import androidx.compose.material.icons.outlined.AttachMoney
 import androidx.compose.material.icons.outlined.SyncAlt
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,6 +34,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -42,172 +48,219 @@ import com.example.smartspend.R
 import com.example.smartspend.Screens.home.ui.theme.SmartSpendTheme
 import com.example.smartspend.data.Categories
 import com.example.smartspend.data.CategoryRepository
+import com.example.smartspend.data.UserRepository
+import com.example.smartspend.data.dataBaseRepository
 import com.example.smartspend.navigation.Routes
+import com.example.smartspend.setCategory
+import kotlin.random.Random
 
 // Main composable function for the Transaction screen
 @Composable
 fun TransactionScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    setCategory(userEmail = UserRepository.getEmail(), context)
+    val category = CategoryRepository.getAllCategories()
 
-    // Box composable with rounded corner shape, background color, and fixed height
-    Box(
-        modifier = Modifier
-            .fillMaxWidth() // Fill the maximum available width
-            .clip(RoundedCornerShape(bottomEnd = 100.dp)) // Apply rounded corner shape to the bottom-end corner
-            .height(300.dp) // Set a fixed height of 400dp
-            .background(color = Color(0xff009177)) // Set the background color to yellow
-    ) {
-        // Image composable inside the Box, filling the maximum size and cropping to fit
-        Image(
-            modifier = Modifier.fillMaxSize(), // Fill the maximum available size
-            painter = painterResource(id = R.drawable.transact), // Set the image resource
-            contentDescription = "happy woman", // Content description for accessibility
-            contentScale = ContentScale.Crop // Crop the image to fit the available space while maintaining aspect ratio
-        )
-    }
 
-    // Column composable for the content below the image
-    Column(
-        modifier = Modifier
-            .fillMaxSize() // Fill the maximum available size
-            .padding(horizontal = 16.dp), // Add horizontal padding
-        //verticalArrangement = Arrangement.Center, // Center the content vertically
-        horizontalAlignment = Alignment.CenterHorizontally // Center the content horizontally
-    ) {
-        Spacer(modifier = Modifier.
-                    size(200.dp)) // Add vertical spacing of 15dp
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xff009177)
+    ){
 
-        // Title text with styling
-        Box (
+
+        // Column composable for the content below the image
+        Column(
             modifier = Modifier
-                .clip(RoundedCornerShape(size = 10.dp))
-                .background(color = Color.White),
-        ){
-            Text(
-                text = "Manage your finances \nANYTIME, ANYWHERE",
-                style = MaterialTheme.typography.headlineLarge.copy(
-                    fontWeight = FontWeight.Bold,
-                    color = Color(219, 132, 11)
-                ),
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(10.dp, 0.dp)
-            )
-        }
+                .fillMaxSize() ,
+            horizontalAlignment = Alignment.CenterHorizontally // Center the content horizontally
+        ) {
+            Spacer(modifier = Modifier.height(30.dp)) // Vertical spacing
 
-        Spacer(modifier = Modifier.height(36.dp)) // Vertical spacing
-        // Subtitle text with styling
-        Text(
-            text = "Secure your financial future with our \ntrusted budgeting services",
-            style = MaterialTheme.typography.bodyLarge.copy(
-                fontWeight = FontWeight.ExtraBold
-            ),
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.inverseOnSurface,
-            modifier = Modifier
-                .padding(10.dp)
-        )
-        Spacer(modifier = Modifier.height(30.dp)) // Vertical spacing
+                Image(
+                    modifier = Modifier
+                        .requiredSize(290.dp, 180.dp)
+                    , // Fill the maximum available size
+                    painter = painterResource(id = R.drawable.transact), // Set the image resource
+                    contentDescription = "happy woman", // Content description for accessibility
+                    contentScale = ContentScale.FillBounds // Crop the image to fit the available space while maintaining aspect ratio
+                )
 
-
-        // Row of buttons with styling
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp, 0.dp),
-            horizontalArrangement = Arrangement.SpaceAround) {
-            //Transfer button
             Box (
                 modifier = Modifier
                     .clip(RoundedCornerShape(size = 10.dp))
-                    .background(color = MaterialTheme.colorScheme.inverseOnSurface)
-                    .padding(5.dp)
-                    .width(90.dp)
-                    .clickable {
+                    .background(color = Color.White),
+            ){
 
-                        navController.navigate(Routes.TransferScreen.routes)
+                Column {
 
-                    }
-                    .shadow(
-                        elevation = 10.dp,
-                        spotColor = MaterialTheme.colorScheme.onBackground,
-                        shape = RoundedCornerShape(1.dp),
-                        ambientColor = MaterialTheme.colorScheme.onBackground
+                }
+                Text(
+                    text = "Manage your finances \nANYTIME, ANYWHERE",
+                    style = MaterialTheme.typography.headlineLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = Color(219, 132, 11)
                     ),
-
-                ){
-                Row {
-                    Icon(imageVector = Icons.Outlined.SyncAlt, contentDescription = "")
-                    Text(text = "Transfer",
-                        fontWeight = FontWeight.W400,
-                        color = MaterialTheme.colorScheme.onBackground,
-                        fontSize = 15.sp,fontFamily = FontFamily.SansSerif
-                    )
-                }
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(10.dp, 0.dp)
+                )
             }
 
-            //Pay button
-            Box (
+            Spacer(modifier = Modifier.height(16.dp)) // Vertical spacing
+            // Subtitle text with styling
+            Text(
+                text = "Secure your financial future with our \ntrusted budgeting services",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ),
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.inverseOnSurface,
                 modifier = Modifier
-                    .clip(RoundedCornerShape(size = 10.dp))
-                    .background(color = MaterialTheme.colorScheme.inverseOnSurface)
-                    .padding(5.dp)
-                    .width(90.dp)
-                    .clickable {
+                    .padding(10.dp)
+            )
+            Spacer(modifier = Modifier.height(30.dp)) // Vertical spacing
 
-                        navController.navigate(Routes.PayScreen.routes)
 
+            // Row of buttons with styling
+            Row(modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround) {
+                //Transfer button
+                Box (
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(size = 10.dp))
+                        .background(color = MaterialTheme.colorScheme.inverseOnSurface)
+                        .padding(5.dp)
+                        .width(90.dp)
+                        .clickable {
+
+                            navController.navigate(Routes.TransferScreen.routes)
+
+                        }
+                        .shadow(
+                            elevation = 10.dp,
+                            spotColor = MaterialTheme.colorScheme.onBackground,
+                            shape = RoundedCornerShape(1.dp),
+                            ambientColor = MaterialTheme.colorScheme.onBackground
+                        ),
+
+                    ){
+                    Row {
+                        Icon(imageVector = Icons.Outlined.SyncAlt, contentDescription = "")
+                        Text(text = "Transfer",
+                            fontWeight = FontWeight.W400,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontSize = 15.sp,fontFamily = FontFamily.SansSerif
+                        )
                     }
-                    .shadow(
-                        elevation = 10.dp,
-                        spotColor = MaterialTheme.colorScheme.onBackground,
-                        shape = RoundedCornerShape(1.dp),
-                        ambientColor = MaterialTheme.colorScheme.onBackground
-                    )
-            ){
-                Row {
-                    Icon(imageVector = Icons.Outlined.AttachMoney, contentDescription = "")
-                    Text(text = "Pay",
-                        fontWeight = FontWeight.W400,
-                        color = MaterialTheme.colorScheme.onBackground,
-                        fontSize = 15.sp,fontFamily = FontFamily.SansSerif
-                    )
+                }
+
+                //Pay button
+                Box (
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(size = 10.dp))
+                        .background(color = MaterialTheme.colorScheme.inverseOnSurface)
+                        .padding(5.dp)
+                        .width(90.dp)
+                        .clickable {
+
+                            navController.navigate(Routes.PayScreen.routes)
+
+                        }
+                        .shadow(
+                            elevation = 10.dp,
+                            spotColor = MaterialTheme.colorScheme.onBackground,
+                            shape = RoundedCornerShape(1.dp),
+                            ambientColor = MaterialTheme.colorScheme.onBackground
+                        )
+                ){
+                    Row {
+                        Icon(imageVector = Icons.Outlined.AttachMoney, contentDescription = "")
+                        Text(text = "Pay",
+                            fontWeight = FontWeight.W400,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontSize = 15.sp,fontFamily = FontFamily.SansSerif
+                        )
+                    }
+                }
+
+                //Buy button
+                Box (
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(size = 10.dp))
+                        .background(color = MaterialTheme.colorScheme.inverseOnSurface)
+                        .padding(5.dp)
+                        .width(90.dp)
+                        .clickable { navController.navigate(Routes.BuyScreen.routes) }
+                        .shadow(
+                            elevation = 10.dp,
+                            spotColor = MaterialTheme.colorScheme.onBackground,
+                            shape = RoundedCornerShape(1.dp),
+                            ambientColor = MaterialTheme.colorScheme.onBackground
+                        )
+                ){
+                    Row {
+                        Icon(imageVector = Icons.Outlined.AccountBalanceWallet, contentDescription = "")
+                        Text(text = "Buy",
+                            fontWeight = FontWeight.W400,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontSize = 15.sp,fontFamily = FontFamily.SansSerif
+                        )
+                    }
                 }
             }
+            Spacer(modifier = Modifier.height(30.dp)) // Vertical spacing
 
-            //Buy button
-            Box (
+            Column (
                 modifier = Modifier
-                    .clip(RoundedCornerShape(size = 10.dp))
-                    .background(color = MaterialTheme.colorScheme.inverseOnSurface)
-                    .padding(5.dp)
-                    .width(90.dp)
-                    .clickable { navController.navigate(Routes.BuyScreen.routes) }
-                    .shadow(
-                        elevation = 10.dp,
-                        spotColor = MaterialTheme.colorScheme.onBackground,
-                        shape = RoundedCornerShape(1.dp),
-                        ambientColor = MaterialTheme.colorScheme.onBackground
-                    )
+                    .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+                    .fillMaxSize()
+                    .background(color = MaterialTheme.colorScheme.background),
+                horizontalAlignment = Alignment.CenterHorizontally
             ){
-                Row {
-                    Icon(imageVector = Icons.Outlined.AccountBalanceWallet, contentDescription = "")
-                    Text(text = "Buy",
-                        fontWeight = FontWeight.W400,
-                        color = MaterialTheme.colorScheme.onBackground,
-                        fontSize = 15.sp,fontFamily = FontFamily.SansSerif
-                    )
+                Spacer(modifier = Modifier.height(20.dp))
+                Row (
+                    modifier = Modifier.fillMaxWidth().padding(10.dp, 0.dp),
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    Box (modifier = Modifier.width(150.dp)){
+                        Text(text = "Categories", fontWeight = FontWeight.Bold)
+                    }
+                    Box (modifier = Modifier.width(120.dp)){
+                        Text(text = "Amount", fontWeight = FontWeight.Bold)
+                    }
+                    Box (modifier = Modifier.width(100.dp)){
+                        Text(text = "Spent", fontWeight = FontWeight.Bold)
+                    }
                 }
-            }
-        }
-        Spacer(modifier = Modifier.height(30.dp)) // Vertical spacing
+                Spacer(modifier = Modifier.height(10.dp))
 
-        Column {
-            Row {
+
+
+                category.forEach {result ->
+                    Row (
+                        modifier = Modifier.fillMaxWidth().padding(10.dp, 0.dp),
+                        horizontalArrangement = Arrangement.Start
+                    ){
+                        Box (modifier = Modifier.width(150.dp)){
+                            Text(text = "${result.name}", fontWeight = FontWeight.Bold, color = Color(0xff009177))
+                        }
+                        Box (modifier = Modifier.width(120.dp)){
+                            Text(text = "N$ ${result.amount}", fontWeight = FontWeight.Bold)
+                            Text(text = "N$", fontWeight = FontWeight.Bold, color = Color.Green)
+                        }
+                        Box {
+                            Text(text = "N$ ${result.spend}", fontWeight = FontWeight.Bold)
+                            Text(text = "N$", fontWeight = FontWeight.Bold, color = Color.Red)
+                        }
+                    }
+                }
             }
         }
     }
-}
 
+}
 // Preview for Transaction screen
 @Preview(showBackground = true)
 @Composable
